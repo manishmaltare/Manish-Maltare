36b58eb407b98781d22cf2dec16f75968fbe7679
manishmaltare <manishmaltare@gmail.com>
2025-08-31 13:22:34 +0530

    Update assignment_logisticregression_deployment_final.py

--- assignment_logisticregression_deployment_final.py
+++ assignment_logisticregression_deployment_final.py
@@ -249,43 +249,6 @@ from sklearn.metrics import roc_curve, auc
 y_pred_new=model.predict(target)
 y_pred_new
 
-# ROC - AUC SCORE
-y_prob=model.predict_proba(x_test)[:,1]
-y_prob
-
-y_prob.shape
-
-import matplotlib.pyplot as plt
-from sklearn.metrics import roc_curve
-
-fpr, tpr, threshold = roc_curve(y_test,y_prob)
-
-roc_auc=auc(fpr, tpr)
-print(roc_auc*100)
-
-plt.figure(figsize=(8, 6))
-plt.plot(fpr, tpr, color='blue', lw=2, label=f"ROC Curve (AUC = {roc_auc:.2f})")
-plt.plot([0, 1], [0, 1], color='gray', linestyle='--')  # Diagonal line
-plt.xlabel('False Positive Rate')
-plt.ylabel('True Positive Rate')
-plt.title('Receiver Operating Characteristic (ROC) Curve')
-plt.legend(loc="lower right")
-plt.grid(True)
-plt.show()
-# AUC Accuracy is 88%,
-# the rate of True positive is high and False positive is low.
-# as the curve is above diagonal line, means the model is perfect.
-
-# CROSS VALIDATION
-from sklearn.model_selection import KFold,cross_val_score
-
-kf=KFold(n_splits=10,shuffle=True, random_state=42)
-
-score = cross_val_score(model,x,y,cv=kf,scoring='accuracy')
-
-print(score.mean()*100)
-# Cross validation score is = 79%
-
 # Deployment :
 
 model
