cc9780ed311a81993a1ad6404969a0a163b9194e
manishmaltare <manishmaltare@gmail.com>
2025-09-04 19:51:36 +0530

    Update pickle_of_assignment_logisticregression_deployment_final.py

--- pickle_of_assignment_logisticregression_deployment_final.py
+++ pickle_of_assignment_logisticregression_deployment_final.py
@@ -94,8 +94,6 @@ from sklearn.model_selection import train_test_split
 
 x_train,x_test,y_train,y_test=train_test_split(x,y,random_state=42,train_size=0.8)
 
-x_train.shape,x_test.shape,y_train.shape,y_test.shape
-
 column_names=x_train.columns
 
 from sklearn.preprocessing import StandardScaler
