a7dc1eea378c9b25552eb0a2385749c5b097f213
manishmaltare <manishmaltare@gmail.com>
2025-09-04 21:24:51 +0530

    Update prefinal_pickle_of_assignment_logisticregression_deployment.py

--- prefinal_pickle_of_assignment_logisticregression_deployment.py
+++ prefinal_pickle_of_assignment_logisticregression_deployment.py
@@ -112,6 +112,7 @@ import pandas as pd
 import streamlit as st
 import pickle
 import numpy as np
+import pandas as pd
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 
 # Load the trained model
